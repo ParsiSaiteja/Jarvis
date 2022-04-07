0387c309afb3284807240463d9f86645a5278753
Saiteja <91063931+ParsiSaiteja@users.noreply.github.com>
2022-04-07 21:59:02 +0530

    Update voices.py

--- voices.py
+++ voices.py
@@ -1,5 +1,5 @@
 import pyttsx3    # IT IS USED TO CONVERT THE TEXT TO SPEECH 
-import pyaudio    # IT IS USED TO SUPPORT THE AUDIO
+import pyaudio    # IT IS USED TO SUPPORT THE AUDIO. PYAUDIO IS ONLY SUPPORTED ON PYTHON 3.6 VERSION
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
