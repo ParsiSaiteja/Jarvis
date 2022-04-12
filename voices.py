20de25fb9fba021bf03c3ae719372e7f128a3d17
Saiteja <91063931+ParsiSaiteja@users.noreply.github.com>
2022-04-12 12:11:48 +0530

    Update voices.py

--- voices.py
+++ voices.py
@@ -7,5 +7,5 @@ voices = engine.getProperty('voices')
 for voice in voices:
     print(voice.id)
     engine.setProperty('voice', voice.id)
-    engine.say("Hello Sir i am your virtual assistant")
+    engine.say("Hello Sir i am your virtual assistant it is an jarvis project")
 engine.runAndWait()
